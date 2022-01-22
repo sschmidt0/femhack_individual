33e0927bbb4b88040532200427830b498b65a1b9
sarah <sschmidt0@uoc.edu>
2022-01-22 18:16:58 +0100

    corrected email regex

--- src/assets/validateRegisterLoginInput.js
+++ src/assets/validateRegisterLoginInput.js
@@ -3,7 +3,7 @@ const isEmpty = require('is-empty');
 
 export const validateInput = (data) => {
   const errors = {};
-  const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const emailRegEx = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
   // Convert empty fields to an empty string so we can use validator functions
   data.userName = !isEmpty(data.userName) ? data.userName : "";
